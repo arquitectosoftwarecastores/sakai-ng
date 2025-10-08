579b9cb9e491034e2b3677d43c237f68883a57c5
arquitectosoftware1 <arquitectosoftware1_lem@castores.com.mx>
2025-10-08 11:52:45 -0600

    patch: se actualiza configuracion de sidebar para que sea responsivo

--- src/app/layout/component/app.sidebar.ts
+++ src/app/layout/component/app.sidebar.ts
@@ -12,9 +12,10 @@ import { SimpleProfileCardComponent } from "./simple-profile-card/simple-profile
         <div class="border-b border-gray-200">
                 <app-simple-profile-card />
             </div>
-        <p-scrollpanel [style]="{ height: '60vh' }">
+        <p-scrollpanel styleClass="scrollpanel">
             <app-menu></app-menu>
         </p-scrollpanel>
+        <div class="float-end mr-2 mt-4 text-primary">v0.0.0.0</div>
     </div>`
 })
 export class AppSidebar {
