dd8f9f1bbbbcc69fef8dbc4cb4fd946a43c6b52b
arquitectosoftware1 <arquitectosoftware1_lem@castores.com.mx>
2026-02-06 14:08:08 -0600

    test : workflow 18

--- eslint.config.js
+++ eslint.config.js
@@ -1,87 +1,89 @@
-// eslint.config.js
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import angular from 'angular-eslint';
-import prettierPlugin from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
-
-export default tseslint.config(
-    {
-        ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**'],
+export default {
+    root: true,
+    ignorePatterns: ['**/dist/**'],
+    plugins: ['prettier'],
+    extends: ['prettier'],
+    rules: {
+        'padding-line-between-statements': [
+            'error',
+            { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+            { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+            { blankLine: 'any', prev: ['case', 'default'], next: 'break' },
+            { blankLine: 'any', prev: 'case', next: 'case' },
+            { blankLine: 'always', prev: '*', next: 'return' },
+            { blankLine: 'always', prev: 'block', next: '*' },
+            { blankLine: 'always', prev: '*', next: 'block' },
+            { blankLine: 'always', prev: 'block-like', next: '*' },
+            { blankLine: 'always', prev: '*', next: 'block-like' },
+            { blankLine: 'always', prev: ['import'], next: ['const', 'let', 'var'] }
+        ]
     },
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...tseslint.configs.stylistic,
-    ...angular.configs.tsRecommended,
-    {
-        files: ['src/**/*.ts'],
-        languageOptions: {
+    overrides: [
+        {
+            files: ['*.ts'],
             parserOptions: {
-                project: ['./tsconfig.json'],
-                createDefaultProgram: true,
+                project: ['tsconfig.json', 'e2e/tsconfig.json'],
+                createDefaultProgram: true
             },
+            extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@angular-eslint/recommended', 'plugin:@angular-eslint/template/process-inline-templates', 'prettier'],
+            rules: {
+                '@angular-eslint/component-selector': [
+                    'error',
+                    {
+                        type: 'element',
+                        prefix: 'p',
+                        style: 'kebab-case'
+                    }
+                ],
+                '@angular-eslint/directive-selector': [
+                    'error',
+                    {
+                        type: 'attribute',
+                        prefix: 'p',
+                        style: 'camelCase'
+                    }
+                ],
+                '@angular-eslint/component-class-suffix': [
+                    'error',
+                    {
+                        suffixes: ['']
+                    }
+                ],
+                '@angular-eslint/template/eqeqeq': [
+                    'error',
+                    {
+                        allowNullOrUndefined: true
+                    }
+                ],
+                '@angular-eslint/no-host-metadata-property': 'off',
+                '@angular-eslint/no-output-on-prefix': 'off',
+                '@typescript-eslint/ban-types': 'off',
+                '@typescript-eslint/no-explicit-any': 'off',
+                '@typescript-eslint/no-inferrable-types': 'off',
+                'arrow-body-style': ['error', 'as-needed'],
+                curly: 0,
+                '@typescript-eslint/member-ordering': [
+                    'error',
+                    {
+                        default: ['public-static-field', 'static-field', 'instance-field', 'public-instance-method', 'public-static-field']
+                    }
+                ],
+                'no-console': 0,
+                'prefer-const': 0
+            }
         },
-        plugins: {
-            'prettier': prettierPlugin
+        {
+            files: ['*.html'],
+            extends: ['plugin:@angular-eslint/template/recommended', 'prettier'],
+            rules: {}
         },
-        rules: {
-            "no-console": "error",
-            "@typescript-eslint/naming-convention": [
-                "error",
-                
-                {
-                    selector: 'variable',
-                    format: 'camelCase',
-                    message: 'Las variables deben ir en camelCase.'
-                },
-                {
-                    selector: 'variable',
-                    type: 'array',
-                    filter: {
-                        regex: '^lst',
-                        match: true
-                    },
-                    format: 'camelCase',
-                    prefix: ['lst'],
-                    message: 'Los arrays deben llevar el prefijo "lst" (ej: lstClientes).'
-                },
-                {
-                    selector: 'class',
-                    format: 'PascalCase',
-                    message: 'Las clases deben ir en PascalCase.'
+        {
+            files: ['*.js'],
+            rules: {
+                parserOptions: {
+                    allowImportExportEverywhere: true
                 }
-            ],
-            'padding-line-between-statements': [
-                'error',
-                { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
-                { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
-                { blankLine: 'any', prev: ['case', 'default'], next: 'break' },
-                { blankLine: 'always', prev: '*', next: 'return' },
-                { blankLine: 'always', prev: 'block', next: '*' },
-                { blankLine: 'always', prev: '*', next: 'block' },
-                { blankLine: 'always', prev: 'import', next: ['const', 'let', 'var'] }
-            ],
-
-            '@angular-eslint/component-selector': [
-                'error',
-                { type: 'element', prefix: 'p', style: 'kebab-case' }
-            ],
-            '@angular-eslint/directive-selector': [
-                'error',
-                { type: 'attribute', prefix: 'p', style: 'camelCase' }
-            ],
-
-            'no-console': 'error', 
-            '@angular-eslint/component-class-suffix': ['error', { suffixes: ['Component', 'Page'] }], 
-
-            ...prettierConfig.rules,
-        },
-    },
-    {
-        files: ['src/**/*.html'],
-        extends: [...angular.configs.templateRecommended],
-        rules: {
-            '@angular-eslint/template/eqeqeq': ['error', { allowNullOrUndefined: true }]
-        },
-    }
-);
+            }
+        }
+    ]
+};
